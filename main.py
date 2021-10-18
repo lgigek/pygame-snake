8f998d5b9fd50e479efc09d4eac5d72ed833c730
lgigek <lucasgigek@gmail.com>
2021-10-18 15:01:37 -0300

    fixed bug where snake moves only when a key is pressed

--- main.py
+++ main.py
@@ -26,17 +26,17 @@ screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Snake")
 clock = pygame.time.Clock()
 
-# snake properties
-snake_position = [(200, 200), (210, 200), (220, 200)]  # initial position
-snake_body = pygame.Surface((10, 10))  # defining body type
-snake_body.fill((255, 255, 255))  # setting a color to snake_body
-
 # snake direction
 UP = 0
 RIGHT = 1
 DOWN = 2
 LEFT = 3
-snake_direction = LEFT
+
+# snake properties
+snake_position = [(200, 200), (210, 200), (220, 200)]  # initial position
+snake_body = pygame.Surface((10, 10))  # defining body type
+snake_body.fill((255, 255, 255))  # setting a color to snake_body
+snake_direction = RIGHT  # setting a default direction
 
 # apple properties
 apple_position = on_grid_random()
@@ -60,33 +60,33 @@ while True:
             if event.key == K_RIGHT:
                 snake_direction = RIGHT
 
-        # moves the snake's head according to its direction
-        if snake_direction == UP:
-            snake_position[0] = (snake_position[0][0], snake_position[0][1] - 10)
-        if snake_direction == DOWN:
-            snake_position[0] = (snake_position[0][0], snake_position[0][1] + 10)
-        if snake_direction == LEFT:
-            snake_position[0] = (snake_position[0][0] - 10, snake_position[0][1])
-        if snake_direction == RIGHT:
-            snake_position[0] = (snake_position[0][0] + 10, snake_position[0][1])
+    # moves the snake's head according to its direction
+    if snake_direction == UP:
+        snake_position[0] = (snake_position[0][0], snake_position[0][1] - 10)
+    if snake_direction == DOWN:
+        snake_position[0] = (snake_position[0][0], snake_position[0][1] + 10)
+    if snake_direction == LEFT:
+        snake_position[0] = (snake_position[0][0] - 10, snake_position[0][1])
+    if snake_direction == RIGHT:
+        snake_position[0] = (snake_position[0][0] + 10, snake_position[0][1])
 
-        # checking if snake's head and apple have collided
-        if have_collided(snake_position[0], apple_position):
-            apple_position = on_grid_random()  # generates a new apple
-            snake_position.append((0, 0))  # increase snake
+    # checking if snake's head and apple have collided
+    if have_collided(snake_position[0], apple_position):
+        apple_position = on_grid_random()  # generates a new apple
+        snake_position.append((0, 0))  # increase snake
 
-        # makes the snake move forward
-        for i in range(len(snake_position) - 1, 0, -1):
-            snake_position[i] = (snake_position[i - 1][0], snake_position[i - 1][1])
+    # makes the snake move forward
+    for i in range(len(snake_position) - 1, 0, -1):
+        snake_position[i] = (snake_position[i - 1][0], snake_position[i - 1][1])
 
-        # "cleans" the screen
-        screen.fill((0, 0, 0))
+    # "cleans" the screen
+    screen.fill((0, 0, 0))
 
-        # printing apple
-        screen.blit(apple, apple_position)
+    # printing apple
+    screen.blit(apple, apple_position)
 
-        # "printing" the snake_body for each snake_position
-        for position in snake_position:
-            screen.blit(snake_body, position)
+    # "printing" the snake_body for each snake_position
+    for position in snake_position:
+        screen.blit(snake_body, position)
 
-        pygame.display.update()
+    pygame.display.update()
